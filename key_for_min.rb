041cc51fda5867689901b0e392884254e2c882d1
Chelsey <cswilik@gmail.com>
2020-10-12 17:56:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,31 +14,21 @@ def key_for_min_value(name_hash)
   return min_key
 end 
 
+
+
+
 # def key_for_min_value(name_hash)
-#   array_ordered_by_age = []
-#   array_ordered_by_age = name_hash.collect {|name, age|
-#     age}.sort
-#   name_hash.each do |name, age|
-#     if age == array_ordered_by_age[0]
-#       return name
+#   lowest_value = 0
+#   lowest_key = nil
+  
+#   name_hash.each do |key,value|
+#     #if the lowest_value is in it's intial state  
+#     #or if the value of the current key is less than the lowest_value
+#     #then: set the lowest_value to value and lowest_key to key
+#     if lowest_value == 0 || value < lowest_value
+#       lowest_value = value 
+#       lowest_key = key
 #     end
 #   end
-#   nil 
+#   lowest_key
 # end
-
-
-def key_for_min_value(name_hash)
-  lowest_value = 0
-  lowest_key = nil
-  
-  name_hash.each do |key,value|
-    #if the lowest_value is in it's intial state  
-    #or if the value of the current key is less than the lowest_value
-    #then: set the lowest_value to value and lowest_key to key
-    if lowest_value == 0 || value < lowest_value
-      lowest_value = value 
-      lowest_key = key
-    end
-  end
-  lowest_key
-end
