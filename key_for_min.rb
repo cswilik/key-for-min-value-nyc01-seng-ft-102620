b09089ceb5b217f825f54359fe48516be424e12e
Chelsey <cswilik@gmail.com>
2020-10-12 17:34:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value| 
-    if name_hash[key] = -45 
+    if name_hash[key] == -45 
       puts name_hash[key]
     end 
   end 
