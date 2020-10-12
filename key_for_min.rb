f37d899882e9253abb052bc545fe237f489fd867
Chelsey <cswilik@gmail.com>
2020-10-12 17:15:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,11 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value > name_hash[key][value]
       puts name_hash[key]
+    else name_hash == nil 
+      return nil 
     end 
   end 
-end
+end 
+
 
 key_for_min_value(ikea)
