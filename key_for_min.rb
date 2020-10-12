a81d790c630e34e7d217308d9fc9fe0950b338ff
Chelsey <cswilik@gmail.com>
2020-10-12 17:44:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,7 @@ def key_for_min_value(name_hash)
   min_value = 10000
   name_hash.each do |key, value|
     if min_value == nil || value < min 
-      min_value == value
+      min_value = value
+    else 
+      return nil 
+      
