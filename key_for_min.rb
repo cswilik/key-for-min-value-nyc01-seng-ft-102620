a44da5374b93143778c171ad3cdf8f5a6a4f5d07
Chelsey <cswilik@gmail.com>
2020-10-12 17:56:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,15 @@
 
 
 def key_for_min_value(name_hash)
-  min_value = nil
+  min_key = nil 
+  min_value = 0
   name_hash.each do |key, value|
-    if min_value == nil || value < min_value
+    if min_value == 0 || value < min_value
       min_value = value
-    else 
-      return nil 
+      min_key = key
     end 
-    return key 
   end 
+  return min_key
 end 
 
 # def key_for_min_value(name_hash)
