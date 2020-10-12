f2269000ecb6eed9056903369a584fa70bb0b186
Chelsey <cswilik@gmail.com>
2020-10-12 17:11:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   smallest_value = name_hash[0][0]
   name_hash.each do |key, value|
-    if smallest_value > name_hash[key][vlaue]
+    if smallest_value > name_hash[key][value]
       puts name_hash[key]
     end 
   end 
