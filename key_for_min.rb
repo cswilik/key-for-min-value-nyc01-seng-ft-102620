d270adbf1e4fbf3db0d2f520001cafe609a3cd3d
Chelsey <cswilik@gmail.com>
2020-10-12 17:54:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,17 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 
-# def key_for_min_value(name_hash)
-#   min_value = nil
-#   name_hash.each do |key, value|
-#     if min_value == nil || value < min_value
-#       min_value = value
-#     else 
-#       return nil 
-#     end 
-#     return key 
-#   end 
-# end 
+def key_for_min_value(name_hash)
+  min_value = nil
+  name_hash.each do |key, value|
+    if min_value == nil || value < min_value
+      min_value = value
+    else 
+      return nil 
+    end 
+    return key 
+  end 
+end 
 
 # def key_for_min_value(name_hash)
 #   array_ordered_by_age = []
